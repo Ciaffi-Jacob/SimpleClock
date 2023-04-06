c4188170bf52f839abc6bf63b192d78328e1099c
Jacob C <jacobc@Zipcoders-MacBook-Pro.local>
2023-04-06 19:53:54 -0400

    added buttons

--- SimpleClock.java
+++ SimpleClock.java
@@ -2,8 +2,10 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.TimeZone;
 
 
 public class SimpleClock extends JFrame {
@@ -12,13 +14,19 @@ public class SimpleClock extends JFrame {
         SimpleDateFormat timeFormat;
         SimpleDateFormat dayFormat;
         SimpleDateFormat dateFormat;
-    
+        JButton twelveTwentyFour;
+        JButton timeZone;
+
+
+        boolean timeFlag = false;
+        boolean zoneFlag = false;
         JLabel timeLabel;
         JLabel dayLabel;
         JLabel dateLabel;
         String time;
         String day;
         String date;
+
 
         SimpleClock() {
             this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -40,17 +48,43 @@ public class SimpleClock extends JFrame {
     
             dateLabel=new JLabel();
             dateLabel.setFont(new Font("Ink Free",Font.BOLD,30));
-    
+            twelveTwentyFour = new JButton("12/24");
+            twelveTwentyFour.addActionListener(this::changeHour);
+            timeZone = new JButton("local/GMT");
+            timeZone.addActionListener(this::changeZone);
+
     
             this.add(timeLabel);
             this.add(dayLabel);
             this.add(dateLabel);
+            this.add(timeZone);
+            this.add(twelveTwentyFour);
             this.setVisible(true);
     
             setTimer();
         }
-    
-        public void setTimer() {
+
+    private void changeZone(ActionEvent actionEvent) {
+            if(zoneFlag){
+                timeFormat.setTimeZone(TimeZone.getDefault());
+                zoneFlag = false;
+            } else {
+                timeFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+                zoneFlag = true;
+            }
+    }
+
+    private void changeHour(ActionEvent actionEvent) {
+        if (timeFlag) {
+            timeFormat = new SimpleDateFormat("HH:mm:ss");
+            timeFlag = false;
+        } else {
+            timeFormat = new SimpleDateFormat("hh:mm:ss a");
+            timeFlag = true;
+        }
+    }
+
+    public void setTimer() {
             while (true) {
                 time = timeFormat.format(Calendar.getInstance().getTime());
                 timeLabel.setText(time);
@@ -62,7 +96,7 @@ public class SimpleClock extends JFrame {
                 dateLabel.setText(date);
     
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(1);
                 } catch (Exception e) {
                     e.getStackTrace();
                 }
